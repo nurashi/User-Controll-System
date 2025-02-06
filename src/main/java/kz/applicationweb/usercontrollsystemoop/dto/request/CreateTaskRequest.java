015b207144709db3b00594c8e97bea3a0e4eda42
Nurasyl Orazbek <nur1k.csgo1@gmail.com>
2025-02-07 02:37:13 +0500

    add validators to task

--- src/main/java/kz/applicationweb/usercontrollsystemoop/dto/request/CreateTaskRequest.java
+++ src/main/java/kz/applicationweb/usercontrollsystemoop/dto/request/CreateTaskRequest.java
@@ -1,9 +1,16 @@
 package kz.applicationweb.usercontrollsystemoop.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
 public class CreateTaskRequest {
+    @NotBlank(message = "Title is required")
     private String title;
+    @NotBlank(message = "Description is required")
     private String description;
+    @Positive(message = "Employee id is required")
     private Long employeeId;
+    @Positive(message = "Status id is required")
     private Long statusId;
 
     public CreateTaskRequest(String title, String description, Long employeeId, Long statusId) {
@@ -13,28 +20,35 @@ public class CreateTaskRequest {
         this.statusId = statusId;
     }
 
-    public String getTitle() {
+    public @NotBlank(message = "Title is required") String getTitle() {
         return title;
     }
-    public void setTitle(String title) {
+
+    public void setTitle(@NotBlank(message = "Title is required") String title) {
         this.title = title;
     }
-    public String getDescription() {
+
+    public @NotBlank(message = "Description is required") String getDescription() {
         return description;
     }
-    public void setDescription(String description) {
+
+    public void setDescription(@NotBlank(message = "Description is required") String description) {
         this.description = description;
     }
-    public Long getEmployeeId() {
+
+    public @Positive(message = "Employee id is required") Long getEmployeeId() {
         return employeeId;
     }
-    public void setEmployeeId(Long employeeId) {
+
+    public void setEmployeeId(@Positive(message = "Employee id is required") Long employeeId) {
         this.employeeId = employeeId;
     }
-    public Long getStatusId() {
+
+    public @Positive(message = "Status id is required") Long getStatusId() {
         return statusId;
     }
-    public void setStatusId(Long statusId) {
+
+    public void setStatusId(@Positive(message = "Status id is required") Long statusId) {
         this.statusId = statusId;
     }
 }
