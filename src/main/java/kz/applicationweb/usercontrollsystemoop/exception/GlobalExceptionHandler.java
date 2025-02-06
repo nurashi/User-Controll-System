beb0de8d827579c0ff9a9ebba24b2d50b58eab9d
Nurasyl Orazbek <155213060+nurashi@users.noreply.github.com>
2025-02-06 21:44:54 +0500

    Add default runtime exception handler (#23)

--- src/main/java/kz/applicationweb/usercontrollsystemoop/exception/GlobalExceptionHandler.java
+++ src/main/java/kz/applicationweb/usercontrollsystemoop/exception/GlobalExceptionHandler.java
@@ -1,5 +1,16 @@
 package kz.applicationweb.usercontrollsystemoop.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
-    
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
 }
